676f53794c9d6c36143c7202cf1d394e84a36532
ckt1010 <ckt1010@gmail>
2020-02-03 11:58:23 +0800

    Add step1

---
+++ step1/step1.cpp
@@ -0,0 +1,9 @@
+#include "step1.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Version " << STEP1_VERSION_MAJOR << "." << STEP1_VERSION_MINOR << endl;
+    return 0;
+}
